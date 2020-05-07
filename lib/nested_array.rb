9c82bde0d809ae16eaa475e01382e99e7821921d
Percy Major <chipercy@hotmail.com>
2020-05-07 23:10:02 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -2,7 +2,7 @@ require "pry"
 
 # Examples inspired by U. S. National Organic Standards
 
-ORGANIC_PRODUCE = ["Strawberries","Potatoes","Grapes","Avocadoes","Asparagus"],
+ORGANIC_PRODUCE = ["Strawberries","Potatoes","Grapes","Avocadoes","Asparagus"]
 
 CONVENTIONAL_PRODUCE = ["Grapefruit","Pineapple","Oranges","Watermelon","Eggplant"]
 
