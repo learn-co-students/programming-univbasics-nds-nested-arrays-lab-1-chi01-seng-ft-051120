83f122cb012f4006d5eff97953df6de64509be47
Percy Major <chipercy@hotmail.com>
2020-05-08 00:15:25 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,21 +17,8 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
 end
 
-
-
-def matrix_lookup(matrix, row, column)
-  
- 
-  
-  matrix[row][column]
-  
-   # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
-  
- end 
-  
  def assembled_matrix
-   binding.pry
+
 [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
    
   # Build an array that contains both of the above arrays
@@ -40,9 +27,24 @@ def matrix_lookup(matrix, row, column)
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
 end
   
+
+
+def matrix_lookup(matrix, row, column)
+  
+  matrix[row][column]
+  
+   # Given any matrix (array of arrays), a row index and a column index, 
+  # Return the matrix's content at that row and and column
+  
+end 
+  
    
 def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  
+  matrix[row][column] = new_value
+  matrix 
+  
 end
